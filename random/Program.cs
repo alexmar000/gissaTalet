28bb8399876391bd292b69979944931d6aea26e6
alexmar000 <82221090+alexmar000@users.noreply.github.com>
2021-06-13 22:19:55 +0200

    Add files via upload

---
+++ random/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomer = new Random();
+            int slumpTal = randomer.Next(1, 101);
+            string welcome = "Gissa ett tal mellan 1 och 100!";
+            string below = "Högre! Försök igen.";
+            string above = "Lägre! Försök igen.";
+            string right = "Rätt! Talet var ";
+            string rightTwo = ". Bra jobbat!";
+            string userGuess;
+            int intGuess;
+
+            Console.WriteLine(welcome);
+
+            do
+            {
+                userGuess = Console.ReadLine();
+                intGuess = Convert.ToInt32(userGuess);
+
+                if (intGuess == slumpTal)
+                {
+                    Console.WriteLine(right + slumpTal + rightTwo);
+                }
+                else if (intGuess > slumpTal)
+                {
+                    Console.WriteLine(above);
+                }
+                else
+                {
+                    Console.WriteLine(below);
+
+                }
+            }
+            while (intGuess != slumpTal);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
